07df832da612630969ba22934f44c1164d0ff9b1
lola <keepthechange2021@gmail.com>
2024-03-01 06:58:15 -0800

    changed stylles

--- frontend/src/components/Trip/Trip.jsx
+++ frontend/src/components/Trip/Trip.jsx
@@ -9,7 +9,7 @@ const Trip = () => {
   return (
     <div className="px-4 sm:px-10 py-6 grid grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-4 sm:gap-6 bg-white">
       {dataTrips.map((trip) => (
-        <div key={trip.id} className="flex flex-col border-2 rounded-xl overflow-hidden group hover:bg-white hover:shadow-xl transition-all duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-105">
+        <div key={trip.id} className="flex flex-col border-2 rounded-xl overflow-hidden group md:hover:bg-white md:hover:shadow-xl md:transition-all md:duration-300 md:ease-in-out md:hover:-translate-y-1 md:hover:scale-105">
           {/* Trip images */}
           <div className="flex-shrink-0 h-32 sm:h-40 md:h-48 lg:h-48 bg-center bg-cover bg-no-repeat"
                style={{ backgroundImage: `url(${trip.photo})` }}>
@@ -17,34 +17,38 @@ const Trip = () => {
 
           {/* Trip info */}
           <div className="flex flex-col flex-grow p-4 antialiased leading-snug">
-            <div className="mb-2 text-gray-600 font-bold text-base md:text-lg lg:text-xl group-hover:text-teal-500">
+            <div className="mb-2 text-gray-600 font-bold text-base md:text-lg lg:text-xl">
               {trip.title} - {trip.duration}hr
             </div>
 
-            <p className="text-gray-700 flex-grow text-sm md:text-base lg:text-base group-hover:text-vuejs-300">
+            <p className="text-gray-700 flex-grow text-sm">
               Price: ${trip.price} <br/>
               Max Capacity: {trip.capacity}
             </p>
 
-            <p className="text-sm md:text-base lg:text-base font-normal antialiased leading-relaxed text-gray-500 overflow-ellipsis overflow-hidden flex-grow mt-2 group-hover:text-vuejs-300">
-              {trip.description}
-            </p>
+            {/* Trip description and BookButton for larger screens */}
+            <div className="hidden sm:block">
+              <p className="text-sm font-normal antialiased leading-relaxed text-gray-500 overflow-ellipsis overflow-hidden flex-grow mt-2">
+                {trip.description}
+              </p>
+            </div>
 
-            <div className="flex flex-row items-center justify-evenly mt-2">
-              <span>
+            {/* Button row for larger screens */}
+            <div className="mt-2 flex flex-col sm:flex-row items-center justify-center sm:justify-evenly">
+              <span className="sm:block hidden">
                 <BookButton size="small" />
               </span>
               <span>
-              <button
-  onClick={() => navigate('/trip-info')}
-  className="hover:bg-teal-500 text-teal-500 hover:text-white font-semibold
-             lg:px-6 lg:py-2 lg:h-[36px]
-             px-6 py-2 text-sm
-             rounded-md border-2 transition-colors duration-150
-             flex items-center justify-center"
->
-  Details
-</button>
+                <button
+                  onClick={() => navigate('/trip-info')}
+                  className="hover:bg-teal-500 text-teal-500 hover:text-white font-semibold
+                             lg:px-6 lg:py-2 lg:h-[36px]
+                             px-6 py-2 text-sm
+                             rounded-md border-2 transition-colors duration-150
+                             flex items-center justify-center"
+                >
+                  Details
+                </button>
               </span>
             </div>
           </div>
--- frontend/src/components/navbar/Navigation.jsx
+++ frontend/src/components/navbar/Navigation.jsx
@@ -19,7 +19,7 @@ export default function Navigation() {
   const closeMenu = () => setIsOpen(false);
 
   return (
-    <nav className={`flex items-center justify-between p-2 sm:p-3 md:p-4 lg:p-5 absolute top-0 left-0 w-full ${isOpen ? 'bg-gray-700' : 'bg-transparent'} text-white`}>
+    <nav className={`flex items-center justify-between p-2 sm:p-3 md:p-4 lg:p-5 fixed top-0 left-0 w-full ${isOpen ? 'bg-gray-700' : 'bg-transparent'} text-white z-50`}>
       <Link to="/">
         <img className="h-[80px] w-[80px] md:h-28 md:w-28 lg:h-28 lg:w-28 object-contain" src="./logo.png" alt="Logo" />
       </Link>
@@ -28,33 +28,38 @@ export default function Navigation() {
         {isOpen ? <FaTimes /> : <FaBars />}
       </button>
       {/* Links container */}
-      <div className={`md:flex md:items-center ${isOpen ? "block" : "hidden"} absolute md:relative top-16 left-0 md:top-0 w-full md:w-auto bg-gray-700 md:bg-transparent`}>
+      <div className={`flex items-start ${isOpen ? "flex-col absolute top-16 left-0 w-full bg-gray-700 md:bg-transparent py-2" : "hidden"} md:flex md:items-center md:relative md:w-auto`}>
+
         {links.map((link, index) => (
+          <div className={`${isOpen ? "hover:bg-gray-300 w-full" : ""}`}>
           <Link
             key={index}
             to={link}
             onClick={closeMenu}
-            className={`block py-1 px-3 md:py-1 md:px-2 md:text-sm lg:py-2 lg:px-4 lg:text-base ${location === link ? "underline" : ""} ${isOpen ? 'hover:bg-gray-700' : 'hover:scale-105 transform transition-transform duration-150'}
-            ${isOpen && index === 0 ? 'pt-8' : ''} // Add more padding-top to the first link when menu is open
-            ${isOpen && index === links.length - 1 ? 'pb-8' : ''} // Add more padding-bottom to the last link when menu is open
-            `}
+            className={`py-1 px-3 text-sm lg:py-2 lg:px-4 lg:text-base ${location === link ? "underline" : ""} ${isOpen ? 'block' : 'hidden md:block'}`}
           >
             {link === "/" ? "Home" : capitalizeLink(link.split("/")[1])}
           </Link>
-        ))}
-        {!isOpen && (
-          <div className="flex items-center gap-4 lg:gap-5 md:pl-2">
-            <a href="https://www.facebook.com/Codmountain" className="p-2 bg-neutral-800 bg-opacity-50  hover:bg-neutral-900 hover:bg-opacity-50 rounded-full ">
-              <FaFacebookF className="lg:text-xl hover:scale-105 transform transition-transform duration-150"/>
-            </a>
-            <a href="https://www.instagram.com/codmountainfishco" className="p-2 bg-neutral-800 bg-opacity-50  hover:bg-neutral-900 hover:bg-opacity-50 rounded-full">
-              <FaInstagram className="lg:text-xl hover:scale-105 transform transition-transform duration-150"/>
-            </a>
-            <div className="inline-block">
-              <BookButton size="small"/>
-            </div>
           </div>
-        )}
+        ))}
+
+
+        <div className="flex items-center gap-4 lg:gap-5">
+          <a href="https://www.facebook.com/Codmountain" className="p-2 hover:bg-neutral-900 hover:bg-opacity-50 rounded-full ">
+            <FaFacebookF className="text-xl"/>
+          </a>
+          <a href="https://www.instagram.com/codmountainfishco" className="p-2 hover:bg-neutral-900 hover:bg-opacity-50 rounded-full">
+            <FaInstagram className="text-xl"/>
+          </a>
+          {/* Always show BookButton, but adjust positioning based on screen size */}
+          <div className="md:block hidden">
+            <BookButton size="small"/>
+          </div>
+        </div>
+      </div>
+      {/* Fixed BookButton for small screens */}
+      <div className="fixed bottom-4 right-4 md:hidden">
+        <BookButton size="small"/>
       </div>
     </nav>
   );
