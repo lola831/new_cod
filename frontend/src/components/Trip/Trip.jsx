a7c68a27b034a6cba7517bfe9fec94cc98bce95a
lola <keepthechange2021@gmail.com>
2024-02-29 18:52:24 -0800

    ?

--- frontend/src/components/Trip/Trip.jsx
+++ frontend/src/components/Trip/Trip.jsx
@@ -7,9 +7,9 @@ const Trip = () => {
   let navigate = useNavigate();
 
   return (
-    <div className="px-4 sm:px-10 py-6 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 sm:gap-6 bg-white">
+    <div className="px-4 sm:px-10 py-6 grid grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-4 sm:gap-6 bg-white">
       {dataTrips.map((trip) => (
-        <div key={trip.id} className={`flex flex-col border-2 rounded-xl overflow-hidden group md:hover:bg-white md:hover:shadow-xl md:transition-all md:duration-300 md:ease-in-out md:hover:-translate-y-1 md:hover:scale-105`}>
+        <div key={trip.id} className="flex flex-col border-2 rounded-xl overflow-hidden group hover:bg-white hover:shadow-xl transition-all duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-105">
           {/* Trip images */}
           <div className="flex-shrink-0 h-32 sm:h-40 md:h-48 lg:h-48 bg-center bg-cover bg-no-repeat"
                style={{ backgroundImage: `url(${trip.photo})` }}>
@@ -17,16 +17,16 @@ const Trip = () => {
 
           {/* Trip info */}
           <div className="flex flex-col flex-grow p-4 antialiased leading-snug">
-            <div className="mb-2 text-gray-600 font-bold text-base md:text-lg lg:text-xl">
+            <div className="mb-2 text-gray-600 font-bold text-base md:text-lg lg:text-xl group-hover:text-teal-500">
               {trip.title} - {trip.duration}hr
             </div>
 
-            <p className="text-gray-700 flex-grow text-sm md:text-base lg:text-base">
+            <p className="text-gray-700 flex-grow text-sm md:text-base lg:text-base group-hover:text-vuejs-300">
               Price: ${trip.price} <br/>
               Max Capacity: {trip.capacity}
             </p>
 
-            <p className="text-sm md:text-base lg:text-base font-normal antialiased leading-relaxed text-gray-500 overflow-ellipsis overflow-hidden flex-grow mt-2">
+            <p className="text-sm md:text-base lg:text-base font-normal antialiased leading-relaxed text-gray-500 overflow-ellipsis overflow-hidden flex-grow mt-2 group-hover:text-vuejs-300">
               {trip.description}
             </p>
 
@@ -35,16 +35,16 @@ const Trip = () => {
                 <BookButton size="small" />
               </span>
               <span>
-                <button
-                  onClick={() => navigate('/trip-info')}
-                  className="hover:bg-teal-500 text-teal-500 hover:text-white font-semibold
-                             lg:px-6 lg:py-2 lg:h-[36px]
-                             px-6 py-2 text-sm
-                             rounded-md border-2 transition-colors duration-150
-                             flex items-center justify-center"
-                >
-                  Details
-                </button>
+              <button
+  onClick={() => navigate('/trip-info')}
+  className="hover:bg-teal-500 text-teal-500 hover:text-white font-semibold
+             lg:px-6 lg:py-2 lg:h-[36px]
+             px-6 py-2 text-sm
+             rounded-md border-2 transition-colors duration-150
+             flex items-center justify-center"
+>
+  Details
+</button>
               </span>
             </div>
           </div>
