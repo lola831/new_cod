7deee6c599d89f7a691ba79c7e1869b4c2998606
lola <keepthechange2021@gmail.com>
2024-03-01 10:38:40 -0800

    making about section responisve

--- frontend/src/components/About/AboutSection.jsx
+++ frontend/src/components/About/AboutSection.jsx
@@ -18,10 +18,10 @@ const About = () => {
 
           <div className="p-4 w-3/5 lg:w-8/12">
 
-              <h3 className="block mb-2 text-xl antialiased font-semibold leading-snug tracking-normal text-teal-500">
+              <h3 className="block mb-2 text-xl md:text-2xl antialiased font-semibold leading-snug tracking-normal text-teal-500">
               About us
               </h3>
-              <p className="block mb-8 text-sm antialiased font-normal leading-relaxed text-white">
+              <p className="block mb-8 text-sm md:text-base antialiased font-normal leading-relaxed text-white">
               At Cod Mountain Fish Co., we prioritize sustainability, ethical fishing practices, and responsible charter boat operations. We firmly believe that protecting our local waters and
               the fish species we target is of utmost importance.
               <span className='hidden md:block md:pt-2'>
@@ -31,7 +31,7 @@ const About = () => {
               </p>
 
               <a href="/about" className="inline-block"><button
-                  className="flex items-center gap-2 hover:px-2 py-3 text-xs font-bold text-center text-yellow-500 uppercase align-middle transition-all rounded-lg select-none disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none hover:text-teal-500"
+                  className="flex items-center gap-2 hover:px-2 py-3 text-xs md:text-sm font-bold text-center text-yellow-500 uppercase align-middle transition-all rounded-lg select-none disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none hover:text-teal-500"
                   type="button">
                   Read More<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"  stroke="currentColor"
                   strokeWidth="2" className="w-4 h-4">
@@ -44,10 +44,10 @@ const About = () => {
       <div className="relative flex text-gray-700 justify-center flex-row mx-auto bg-white">
           <div className="p-4 w-3/5 lg:w-8/12">
 
-              <h3 className="block mb-2 text-xl antialiased font-semibold leading-snug tracking-normal text-yellow-500">
+              <h3 className="block mb-2 text-xl md:text-2xl antialiased font-semibold leading-snug tracking-normal text-yellow-500">
               About Captain
               </h3>
-              <p className="block mb-8 text-sm antialiased font-normal leading-relaxed text-gray-700">
+              <p className="block mb-8 text-sm md:text-base antialiased font-normal leading-relaxed text-gray-700">
               Captain Greg Webb brings over a decade of dedicated experience and a genuine love for fishing to Cod Mountain Fish Co. His journey in the marine industry began as a deckhand on a charter
               boat in Santa Cruz, California, where he learned the ropes and developed a deep appreciation for the ocean.
               <span className='hidden md:block md:pt-2'>
@@ -58,7 +58,7 @@ const About = () => {
               </p>
 
               <a href="/about" className="inline-block"><button
-                  className="flex items-center gap-2 hover:px-2 py-3 text-xs font-bold text-center text-teal-500 uppercase align-middle transition-all rounded-lg select-none disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none hover:text-yellow-500"
+                  className="flex items-center gap-2 hover:px-2 py-3 text-xs md:text-sm font-bold text-center text-teal-500 uppercase align-middle transition-all rounded-lg select-none disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none hover:text-yellow-500"
                   type="button">
                   Read More<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"  stroke="currentColor"
                   strokeWidth="2" className="w-4 h-4">
@@ -86,10 +86,10 @@ const About = () => {
 
           <div className="p-4 w-3/5 lg:w-8/12">
 
-              <h3 className="block mb-2 text-xl antialiased font-semibold leading-snug tracking-normal text-yellow-500">
+              <h3 className="block mb-2 text-xl md:text-2xl antialiased font-semibold leading-snug tracking-normal text-yellow-500">
               About Boat
               </h3>
-              <p className="block mb-8 text-sm antialiased font-normal leading-relaxed text-white">
+              <p className="block mb-8 text-sm md:text-base antialiased font-normal leading-relaxed text-white">
               Introducing the Sea Marie, the pride and joy of Cod Mountain Fish Co. This remarkable fishing vessel combines power, comfort, and functionality to deliver an
               extraordinary fishing experience in the beautiful waters of Santa Cruz.
               <span className='hidden md:block md-pt-2'>Powered by twin Yamaha 150hp engines, the Sea Marie is ready to take you on thrilling fishing adventures with
@@ -97,7 +97,7 @@ const About = () => {
               </p>
 
               <a href="/about" className="inline-block"><button
-                  className="flex items-center gap-2 hover:px-2 py-3 text-xs font-bold text-center text-grey-700 uppercase align-middle transition-all rounded-lg select-none disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none hover:text-yellow-500"
+                  className="flex items-center gap-2 hover:px-2 py-3 text-xs md:text-sm font-bold text-center text-grey-700 uppercase align-middle transition-all rounded-lg select-none disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none hover:text-yellow-500"
                   type="button">
                   Read More<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"  stroke="currentColor"
                   strokeWidth="2" className="w-4 h-4">
